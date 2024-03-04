ab8fe502bd8610b6876b351a6b46b7f196b23099
lamvd <vuduclam2412@gmail.com>
2024-03-04 15:29:10 +0700

    Finish layout and routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
   resources :microposts
-  resources :users
+  resources :users, except: [:new] do
+    collection do
+      get "signup", to: "users#new"
+    end
+  end
   root 'microposts#index'
 end
