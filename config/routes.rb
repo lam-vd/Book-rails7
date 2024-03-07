a02bc41e4ada2b33063367b4bf79dc7c7a6a6e21
lamvd <vuduclam2412@gmail.com>
2024-03-07 16:39:17 +0700

    Add password reset

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   post "/login", to: "sessions#create"
   delete "/logout", to: "sessions#destroy"
   resources :account_activations, only: [:edit]
+  resources :password_resets, only: [:new, :create, :edit, :update]
   resources :microposts
   resources :users, except: [:new] do
     collection do
